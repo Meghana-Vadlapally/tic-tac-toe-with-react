ca61fca7d8e7493f395c488b3f372c734e66a088
Meghana Vadlapally <mvadlapa@buffalo.edu>
2021-02-01 16:58:42 -0500

    adding all files

---
+++ src/Game.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+import { useLocalStorageState } from "./UseLocalStorageState";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useLocalStorageState("tic-tac-toe:history", [
+    Array(9).fill(null)
+  ]);
+  const [currentStep, setCurrentStep] = useLocalStorageState(
+    "tic-tac-toe:step",
+    0
+  );
+
+  const currentSquares = history[currentStep];
+  const winner = calculateWinner(currentSquares);
+  const nextValue = calculateNextValue(currentSquares);
+  const status = calculateStatus(winner, currentSquares, nextValue);
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) {
+      return;
+    }
+
+    const newHistory = history.slice(0, currentStep + 1);
+    const squares = [...currentSquares];
+
+    squares[square] = nextValue;
+    setHistory([...newHistory, squares]);
+    setCurrentStep(newHistory.length);
+  }
+  function restart() {
+    setSquares(Array(9).fill(null));
+  }
+
+  const moves = history.map((stepSquares, step) => {
+    const desc = step ? `Go to move #${step}` : "Go to game start";
+    const isCurrentStep = step === currentStep;
+    return (
+      <li key={step}>
+        <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
+          {desc} {isCurrentStep ? "(current)" : null}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+    ? `Scratch: Cat's game`
+    : `Next player: ${nextValue}`;
+}
+
+function calculateNextValue(squares) {
+  const xSquaresCount = squares.filter((r) => r === "X").length;
+  const oSquaresCount = squares.filter((r) => r === "O").length;
+  return oSquaresCount === xSquaresCount ? "X" : "O";
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game;
